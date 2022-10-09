8314ebe78358fa82057f070c2e2bb34f91e03fd5
Dusan Jovanov <jovanovdusan1@gmail.com>
2022-10-09 17:32:42 +0200

    v0.3.0 A bunch of stuff

--- src/create.ts
+++ src/create.ts
@@ -1,52 +1,270 @@
-import { css } from "@emotion/css";
+import { css, CSSObject, keyframes as emotionKeyframes } from "@emotion/css";
 import { defaultTheme } from "./defaultTheme";
 
 type GenericTheme = {
   [slot in keyof typeof defaultTheme]: Record<string | number | symbol, any>;
 };
 
-type CssFn = (arg: string | number) => string;
+type CustomFn = (arg: string | number) => string;
+
+type Utils<Theme extends GenericTheme> = {
+  truncate: string;
+  spaceBetweenX: (value: keyof Theme["spacing"]) => string;
+  spaceBetweenY: (value: keyof Theme["spacing"]) => string;
+  spaceBetweenXCustom: (value: string) => string;
+  spaceBetweenYCustom: (value: string) => string;
+};
 
 export function createVanillaStyle<Theme extends GenericTheme>(theme: Theme) {
-  class ProxyOne {
+  const _keyframes = {} as any;
+
+  for (const [name, keyframe] of Object.entries(theme.keyframes)) {
+    _keyframes[name] = emotionKeyframes(keyframe);
+  }
+
+  class Base {
     constructor(selector?: string) {
       this.#selector = selector;
+
+      const inst = this;
+      this.utils = Object.defineProperties(
+        {},
+        {
+          truncate: {
+            get() {
+              return css(
+                inst.#getCssObject(
+                  ["overflow", "textOverflow", "whiteSpace"],
+                  ["hidden", "ellipsis", "nowrap"]
+                )
+              );
+            },
+          },
+        }
+      ) as Utils<Theme>;
+
+      const spaceBetweenFn =
+        (cssProp: string) => (value: keyof Theme["spacing"]) => {
+          if (this.#selector) {
+            return css({
+              [`${this.#selector} > * + *`]: this.#getStyleObject(
+                cssProp,
+                value
+              ),
+            });
+          }
+          return css({
+            "> * + *": this.#getStyleObject(cssProp, value),
+          });
+        };
+      this.utils.spaceBetweenX = spaceBetweenFn("marginLeft");
+      this.utils.spaceBetweenY = spaceBetweenFn("marginTop");
     }
     #selector?: any;
+    utils = {} as Utils<Theme>;
 
-    #p = <ThemeSlot extends keyof Theme>(
+    #custom = (cssProps: string | string[]) => {
+      return {
+        custom: (value: any) => {
+          return css(this.#getCssObject(cssProps, value));
+        },
+      };
+    };
+
+    #p = <ThemeSlot extends keyof GenericTheme>(
       themeSlot: ThemeSlot,
       cssProps?: string | string[],
-      selector?: string
+      getValue?: (name: string) => any
     ) => {
+      const obj = this.#custom(cssProps ?? (themeSlot as string));
       const themeObj: any = theme[themeSlot];
-      const fn = (value: any) => {
-        return css(
-          this.#getCssObject(cssProps ?? (themeSlot as string), value, selector)
-        );
-      };
       const inst = this;
       for (const name of Object.keys(themeObj as {})) {
-        Object.defineProperty(fn, name, {
+        Object.defineProperty(obj, name, {
           get() {
             return css(
               inst.#getCssObject(
                 cssProps ?? (themeSlot as string),
-                themeObj[name],
-                selector
+                typeof getValue === "function" ? getValue(name) : themeObj[name]
               )
             );
           },
         });
       }
-      return fn as CssFn & {
+      return obj as {
+        custom: CustomFn;
+      } & {
         [varName in keyof Theme[ThemeSlot]]: string;
       };
     };
 
+    #getCssObject = (cssProps: string | string[], value: any) => {
+      if (this.#selector) {
+        return {
+          [this.#selector]: this.#getStyleObject(cssProps, value),
+        };
+      }
+      return this.#getStyleObject(cssProps, value);
+    };
+
+    #getStyleObject = (cssProps: string | string[], value: any) => {
+      const styleObj = {} as any;
+      if (Array.isArray(cssProps)) {
+        for (let i = 0; i < cssProps.length; i++) {
+          const cssProp = cssProps[i];
+          styleObj[cssProp] = Array.isArray(value) ? value[i] : value;
+        }
+        return styleObj;
+      }
+      return { [cssProps]: value };
+    };
+
+    css = (cssObject: CSSObject) => {
+      if (this.#selector) {
+        return css({
+          [this.#selector]: cssObject,
+        });
+      }
+      return css(cssObject);
+    };
+
+    #getBorderValues = (cssProp: string, w: any, s: any, c: any, o: any) => {
+      const cssObject = {
+        [`${cssProp}Width`]: w,
+        [`${cssProp}Style`]: s,
+        [`${cssProp}Color`]: c,
+      };
+
+      if (o) {
+        cssObject[`${cssProp}Offset`] = o;
+      }
+
+      return cssObject;
+    };
+
+    #borderAndOutlineShorthand = (
+      prop: "border" | "outline",
+      cssProps: string | string[],
+      values: any[]
+    ) => {
+      let cssObject: any;
+
+      if (Array.isArray(cssProps)) {
+        cssObject = {};
+        for (const cssProp of cssProps) {
+          cssObject = {
+            ...cssObject,
+            ...this.#getBorderValues(
+              cssProp,
+              theme[`${prop}Width`][values[0]],
+              theme[`${prop}Style`][values[1]],
+              theme[`${prop}Color`][values[2]],
+              prop === "outline" ? theme[`${prop}Offset`][values[3]] : undefined
+            ),
+          };
+        }
+      } else {
+        cssObject = {
+          ...this.#getBorderValues(
+            cssProps,
+            theme[`${prop}Width`][values[0]],
+            theme[`${prop}Style`][values[1]],
+            theme[`${prop}Color`][values[2]],
+            prop === "outline" ? theme[`${prop}Offset`][values[3]] : undefined
+          ),
+        };
+      }
+
+      if (this.#selector) {
+        return css({
+          [this.#selector]: css(cssObject),
+        });
+      }
+      return css(cssObject);
+    };
+
+    #borderAndOutlineCustom =
+      (cssProp: string | string[]) => (border: string) => {
+        return css(this.#getCssObject(cssProp, border));
+      };
+
+    #borderShorthand =
+      (cssProps: string | string[]) =>
+      <
+        BW extends keyof Theme["borderWidth"],
+        BS extends keyof Theme["borderStyle"],
+        BC extends keyof Theme["borderColor"]
+      >(
+        borderWidth: BW,
+        borderStyle?: BS,
+        borderColor?: BC
+      ) => {
+        return this.#borderAndOutlineShorthand("border", cssProps, [
+          borderWidth,
+          borderStyle,
+          borderColor,
+        ]);
+      };
+
+    // special border shorthand methods
+    border = {
+      all: this.#borderShorthand("border"),
+      custom: this.#borderAndOutlineCustom("border"),
+      top: this.#borderShorthand("borderTop"),
+      topCustom: this.#borderAndOutlineCustom("borderTop"),
+      bottom: this.#borderShorthand("borderBottom"),
+      bottomCustom: this.#borderAndOutlineCustom("borderBottom"),
+      left: this.#borderShorthand("borderLeft"),
+      leftCustom: this.#borderAndOutlineCustom("borderLeft"),
+      right: this.#borderShorthand("borderRight"),
+      rightCustom: this.#borderAndOutlineCustom("borderRight"),
+      x: this.#borderShorthand(["borderLeft", "borderRight"]),
+      y: this.#borderShorthand(["borderTop", "borderBottom"]),
+      xCustom: this.#borderAndOutlineCustom(["borderLeft", "borderRight"]),
+      yCustom: this.#borderAndOutlineCustom(["borderTop", "borderBottom"]),
+    };
+
+    #outlineShorthand =
+      (cssProps: string | string[]) =>
+      <
+        OW extends keyof Theme["outlineWidth"],
+        OS extends keyof Theme["outlineStyle"],
+        OC extends keyof Theme["outlineColor"],
+        OO extends keyof Theme["outlineOffset"]
+      >(
+        outlineWidth: OW,
+        outlineStyle?: OS,
+        outlineColor?: OC,
+        outlineOffset?: OO
+      ) => {
+        return this.#borderAndOutlineShorthand("outline", cssProps, [
+          outlineWidth,
+          outlineStyle,
+          outlineColor,
+          outlineOffset,
+        ]);
+      };
+
+    // special outline shorthand methods
+    outline = {
+      all: this.#outlineShorthand("outline"),
+      custom: this.#borderAndOutlineCustom("outline"),
+    };
+
+    // special animation methods
+    animation = (value: string) => {
+      return css(this.#getCssObject("animation", value));
+    };
+
+    animationName = this.#p(
+      "keyframes",
+      "animationName",
+      (name) => _keyframes[name]
+    );
+
+    // single value -> single or multiple related properties
     bg = this.#p("backgroundColor");
     color = this.#p("color");
-    borderColor = this.#p("borderColor");
     p = this.#p("padding");
     px = this.#p("padding", ["paddingLeft", "paddingRight"]);
     py = this.#p("padding", ["paddingTop", "paddingBottom"]);
@@ -63,18 +281,51 @@ export function createVanillaStyle<Theme extends GenericTheme>(theme: Theme) {
     mb = this.#p("margin", "marginBottom");
     fontSize = this.#p("fontSize");
     borderRadius = this.#p("borderRadius");
-    border = this.#p("borderWidth");
+    borderTopRadius = this.#p("borderRadius", [
+      "borderTopLeftRadius",
+      "borderTopRightRadius",
+    ]);
+    borderBottomRadius = this.#p("borderRadius", [
+      "borderBottomLeftRadius",
+      "borderBottomRightRadius",
+    ]);
+    borderRightRadius = this.#p("borderRadius", [
+      "borderTopRightRadius",
+      "borderBottomRightRadius",
+    ]);
+    borderLeftRadius = this.#p("borderRadius", [
+      "borderTopLeftRadius",
+      "borderBottomLeftRadius",
+    ]);
+    borderTopRightRadius = this.#p("borderRadius", "borderTopRightRadius");
+    borderTopLeftRadius = this.#p("borderRadius", "borderTopLeftRadius");
+    borderBottomRightRadius = this.#p(
+      "borderRadius",
+      "borderBottomRightRadius"
+    );
+    borderBottomLeftRadius = this.#p("borderRadius", "borderBottomLeftRadius");
+    borderWidth = this.#p("borderWidth");
+    borderStyle = this.#p("borderStyle");
+    borderColor = this.#p("borderColor");
+    borderTopWidth = this.#p("borderWidth", "borderTopWidth");
+    borderBottomWidth = this.#p("borderWidth", "borderBottomWidth");
+    borderLeftWidth = this.#p("borderWidth", "borderLeftWidth");
+    borderRightWidth = this.#p("borderWidth", "borderRightWidth");
+    borderTopStyle = this.#p("borderStyle", "borderTopStyle");
+    borderBottomStyle = this.#p("borderStyle", "borderBottomStyle");
+    borderLeftStyle = this.#p("borderStyle", "borderLeftStyle");
+    borderRightStyle = this.#p("borderStyle", "borderRightStyle");
+    borderTopColor = this.#p("borderColor", "borderTopColor");
+    borderBottomColor = this.#p("borderColor", "borderBottomColor");
+    borderLeftColor = this.#p("borderColor", "borderLeftColor");
+    borderRightColor = this.#p("borderColor", "borderRightColor");
     width = this.#p("width");
     height = this.#p("height");
     lineHeight = this.#p("lineHeight");
     gap = this.#p("gap", "gap");
-    borderTop = this.#p("borderWidth", "borderTopWidth");
-    borderBottom = this.#p("borderWidth", "borderBottomWidth");
     boxShadow = this.#p("boxShadow");
     boxShadowColor = this.#p("colors", "--shadow-color");
     zIndex = this.#p("zIndex");
-    spaceBetweenX = this.#p("spacing", "margin-left", "> * + *");
-    spaceBetweenY = this.#p("spacing", "margin-top", "> * + *");
     minHeight = this.#p("minHeight");
     minWidth = this.#p("minWidth");
     maxHeight = this.#p("maxHeight");
@@ -90,6 +341,14 @@ export function createVanillaStyle<Theme extends GenericTheme>(theme: Theme) {
     transitionDuration = this.#p("transitionDuration");
     transitionProperty = this.#p("transitionProperty");
     transitionTimingFunction = this.#p("transitionTimingFunction");
+    transitionDelay = this.#p("transitionDelay");
+    animationDirection = this.#p("animationDirection");
+    animationDelay = this.#p("animationDelay");
+    animationTimingFunction = this.#p("animationTimingFunction");
+    animationPlayState = this.#p("animationPlayState");
+    animationIterationCount = this.#p("animationIterationCount");
+    animationFillMode = this.#p("animationFillMode");
+    animationDuration = this.#p("animationDuration");
     display = this.#p("display");
     flexWrap = this.#p("flexWrap");
     flexGrow = this.#p("flexGrow");
@@ -103,62 +362,59 @@ export function createVanillaStyle<Theme extends GenericTheme>(theme: Theme) {
     textDecorationStyle = this.#p("textDecorationStyle");
     textTransform = this.#p("textTransform");
     textOverflow = this.#p("textOverflow");
+    overflow = this.#p("overflow");
+    overflowX = this.#p("overflow", "overflowX");
+    overflowY = this.#p("overflow", "overflowY");
     verticalAlign = this.#p("verticalAlign");
     tableLayout = this.#p("tableLayout");
     alignItems = this.#p("alignItems");
+    alignContent = this.#p("alignContent");
+    alignSelf = this.#p("alignSelf");
+    justifyItems = this.#p("justifyItems");
     justifyContent = this.#p("justifyContent");
-    borderStyle = this.#p("borderStyle");
-    fontSmoothing = this.#p("fontSmoothing", [
-      "-webkit-font-smoothing",
-      "-moz-osx-font-smoothing",
-    ]);
-
-    #getCssObject = (
-      cssProps: string | string[],
-      value: any,
-      selector?: string
-    ) => {
-      if (this.#selector || selector) {
-        return {
-          [[this.#selector, selector].filter(Boolean).join(" ")]:
-            this.#getStyleObject(cssProps, value),
-        };
-      }
-      return this.#getStyleObject(cssProps, value);
-    };
-
-    #getStyleObject = (cssProps: string | string[], value: any) => {
-      const styleObj = {} as any;
-      if (typeof value === "object") {
-        for (const cssProp of Object.keys(value)) {
-          styleObj[cssProp] = value[cssProp];
-        }
-        return styleObj;
-      }
-      if (Array.isArray(cssProps)) {
-        for (const cssProp of cssProps) {
-          styleObj[cssProp] =
-            typeof value === "function" ? value(cssProp) : value;
-        }
-        return styleObj;
-      }
-      return { [cssProps]: value };
-    };
+    justifySelf = this.#p("alignSelf");
+    position = this.#p("position");
+    gridTemplateColumns = this.#p("gridTemplateColumns");
+    gridTemplateRows = this.#p("gridTemplateRows");
+    gridColumn = this.#p("gridColumn");
+    gridColumnStart = this.#p("gridColumnStart");
+    gridColumnEnd = this.#p("gridColumnEnd");
+    gridRow = this.#p("gridRow");
+    gridRowStart = this.#p("gridRowStart");
+    gridRowEnd = this.#p("gridRowEnd");
+    order = this.#p("order");
+    flexDirection = this.#p("flexDirection");
+    flexBasis = this.#p("flexBasis");
+    letterSpacing = this.#p("letterSpacing");
+    fill = this.#p("fill");
+    strokeColor = this.#p("strokeColor", "stroke");
+    strokeWidth = this.#p("strokeWidth");
+    cursor = this.#p("cursor");
+    userSelect = this.#p("userSelect");
+    opacity = this.#p("opacity");
+    wordBreak = this.#p("wordBreak");
+    whiteSpace = this.#p("whiteSpace");
+    content = this.#p("content");
+    outlineWidth = this.#p("outlineWidth");
+    outlineColor = this.#p("outlineColor");
+    outlineStyle = this.#p("outlineStyle");
+    outlineOffset = this.#p("outlineOffset");
   }
 
-  class ProxyTwo extends ProxyOne {
+  const selectorCache = {} as Record<string, PseudoSelectors>;
+
+  class PseudoSelectors extends Base {
     constructor(selector?: string) {
       super(selector);
 
       const s = (_selector: string) => {
-        return new ProxyOne(`${selector ?? ""}${_selector}`);
+        return new Base(`${selector ?? ""}${_selector}`);
       };
 
       this.hover = s(":hover");
       this.active = s(":active");
       this.focus = s(":focus");
       this.focusWithin = s(":focus-within");
-      this.dark = new ProxyOne(`.dark & ${selector ?? ""}`);
       this.visited = s(":visited");
       this.disabled = s(":disabled");
       this.enabled = s(":enabled");
@@ -174,56 +430,69 @@ export function createVanillaStyle<Theme extends GenericTheme>(theme: Theme) {
       this.required = s(":required");
       this.valid = s(":valid");
       this.invalid = s(":invalid");
+      this.after = s("::after");
+      this.before = s("::before");
     }
 
-    hover: ProxyOne;
-    active: ProxyOne;
-    focus: ProxyOne;
-    dark: ProxyOne;
-    focusWithin: ProxyOne;
-    visited: ProxyOne;
-    disabled: ProxyOne;
-    enabled: ProxyOne;
-    focusVisible: ProxyOne;
-    target: ProxyOne;
-    firstChild: ProxyOne;
-    lastChild: ProxyOne;
-    onlyChild: ProxyOne;
-    empty: ProxyOne;
-    checked: ProxyOne;
-    indeterminate: ProxyOne;
-    default: ProxyOne;
-    required: ProxyOne;
-    valid: ProxyOne;
-    invalid: ProxyOne;
+    hover: Base;
+    active: Base;
+    focus: Base;
+    focusWithin: Base;
+    visited: Base;
+    disabled: Base;
+    enabled: Base;
+    focusVisible: Base;
+    target: Base;
+    firstChild: Base;
+    lastChild: Base;
+    onlyChild: Base;
+    empty: Base;
+    checked: Base;
+    indeterminate: Base;
+    default: Base;
+    required: Base;
+    valid: Base;
+    invalid: Base;
+    after: Base;
+    before: Base;
   }
 
-  class ProxyThree extends ProxyTwo {
-    constructor() {
-      super();
+  class Selector extends PseudoSelectors {
+    selector = (selector: string) => {
+      if (selectorCache[selector]) {
+        return selectorCache[selector];
+      }
+      selectorCache[selector] = new PseudoSelectors(selector);
+      return selectorCache[selector];
+    };
+  }
 
-      this.screen = {} as Record<keyof typeof theme["screens"], ProxyTwo>;
+  class Dark extends Selector {
+    constructor(selector?: string) {
+      super(selector);
+      this.dark = new Selector(`${selector ?? ""} .dark &`);
+    }
+
+    dark: Selector;
+  }
+
+  class Breakpoints extends Dark {
+    constructor(selector?: string) {
+      super(selector);
+
+      this.screen = {} as Record<keyof typeof theme["screens"], Dark>;
 
       for (const bp of Object.keys(
         theme.screens
       ) as (keyof typeof theme["screens"])[]) {
-        this.screen[bp] = new ProxyTwo(
-          `@media (min-width: ${theme.screens[bp]})`
-        );
+        this.screen[bp] = new Dark(`@media (min-width: ${theme.screens[bp]})`);
       }
     }
-
-    #selectorProxies = {} as Record<string, ProxyTwo>;
-
-    selector = (selector: string) => {
-      if (this.#selectorProxies[selector])
-        return this.#selectorProxies[selector];
-      this.#selectorProxies[selector] = new ProxyTwo(selector);
-      return this.#selectorProxies[selector];
-    };
-
-    screen: Record<keyof typeof theme["screens"], ProxyTwo>;
+    screen: Record<keyof typeof theme["screens"], Dark>;
   }
 
-  return new ProxyThree();
+  return {
+    classes: new Breakpoints(),
+    animationNames: _keyframes,
+  };
 }
